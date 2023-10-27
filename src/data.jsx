8e8228c74f73e8b70bdd3318a38acf7c412caf8e
hashim <hashim00541x@gmail.com>
2023-10-27 11:42:17 +0530

    bg

--- src/data.jsx
+++ src/data.jsx
@@ -1,52 +1,72 @@
+import avengersimg from './assets//images/avengers.jpg'
+import avengerssong from './assets/music/avengers-theme.mp3'
+
+import backimg from './assets/images/back-to-the-future.jpg'
+import backsong from './assets/music/back-to-the-future-theme.mp3'
+
+import indianaimg from './assets/images/indiana-jones.jpg'
+import indianasong from './assets/music/indiana-jones-theme.mp3'
+
+import jurassicimg from './assets/images/jurassic-park.jpg'
+import jurassicsong from './assets/music/jurassic-park-theme.mp3'
+
+import piratesimg from './assets/images/pirates.jpg'
+import piratessong from './assets/music/pirates-of-the-caribbean-theme.mp3'
+
+import spiderimg from './assets/images/spiderman.jpg'
+import spidersong from './assets/music/spiderman-theme.mp3'
+
+import topimg from './assets/images/top-gun.jpg'
+import topsong from './assets/music/top-gun-anthem.mp3'
+
 const data=[
-    
     {
         id:1,
         title:'Avengers',
-        img:'/images/avengers.jpg',
-        song:'/music/avengers-theme.mp3',
+        img:avengersimg,
+        song:avengerssong,
         time:'4 m : 11 s'
     },
     {
         id:2,
         title:'Back to the future',
-        img:'/images/back-to-the-future.jpg',
-        song:'/music/back-to-the-future-theme.mp3',
+        img:backimg,
+        song:backsong,
         time:'3 m : 19 s'
     },
     {
         id:3,
         title:'Indiana jones',
-        img:'/music-player/assets/images/indiana-jones.jpg',
-        song:'/music-player/assets/music/indiana-jones-theme.mp3',
+        img:indianaimg,
+        song:indianasong,
         time:'2 m : 26 s'
     },
     {
         id:4,
         title:'Jurassic park',
-        img:'/music-player/assets/images/jurassic-park.jpg',
-        song:'/music-player/assets/music/jurassic-park-theme.mp3',
+        img:jurassicimg,
+        song:jurassicsong,
         time:'5 m : 20 s'
     },
     {
         id:5,
         title:'Pirates of the caribbean',
-        img:'/music-player/assets/images/pirates.jpg',
-        song:'/music-player/assets/music/pirates-of-the-caribbean-theme.mp3',
+        img:piratesimg,
+        song:piratessong,
         time:'4 m : 36 s'
     },
     {
         id:6,
         title:'Spiderman',
-        img:'/music-player/assets/images/spiderman.jpg',
-        song:'/music-player/assets/music/spiderman-theme.mp3',
+        img:spiderimg,
+        song:spidersong,
         time:'3 m : 43 s'
     },
     {
         id:7,
         title:'Top gun',
-        img:'/music-player/assets/images/top-gun.jpg',
-        song:'/music-player/assets/music/top-gun-anthem.mp3',
+        img:topimg,
+        song:topsong,
         time:'2 m : 28 s'
     }
 ]
